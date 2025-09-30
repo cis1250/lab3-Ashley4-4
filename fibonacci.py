c1d65c5b6935ddef7ac03780d1348f4692764a50
Ashley4-4 <alorenzo@uoguelph.ca>
2025-09-30 17:09:14 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -7,14 +7,18 @@
 # Use a for loop to print the Fibonacci sequence up to that many terms.
 print("How many terms of the fibonacci sequence do you want?")
 num = input()
-if(num <= 0 ):
-   print("please enter a positive number)
+
+while not num.isdigit():
+    print("Please enter a valid number")
+    num = input()
 else:
-   a, b = 0, 1
-   print("Fibonacci Sequence: ")
-   for _ in range(num): 
-       print(a, end=" ")
-       a, b = b, a + b
-       print()
-       
+    num = int(num)
+    if(num <= 0 ):
+       print("Please enter a positive number")
+    else:
+       a, b = 0, 1
+       print("Fibonacci Sequence: ")
+       for _ in range(num): 
+           print(a, end=" ")
+           a, b = b, a + b
    
